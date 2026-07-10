cf9041833fa3f7f30df623dd7873d73ca63c91df
teksite <teksite.design@gmail.com>
2026-07-10 15:51:52 +0330

    change

--- src/resources/assets/browser/ui/SelectionGridUi.js
+++ src/resources/assets/browser/ui/SelectionGridUi.js
@@ -18,7 +18,6 @@ export default class SelectionGridUi extends UiService {
         return !!this.gridEl;
     }
 
-
     busEvents() {
 
         return {
@@ -41,7 +40,7 @@ export default class SelectionGridUi extends UiService {
         ];
     }
 
-
+    
     renderSelections() {
 
         const files = this.state.get("select.files", {});
